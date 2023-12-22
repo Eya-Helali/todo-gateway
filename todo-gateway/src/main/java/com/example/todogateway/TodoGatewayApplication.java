57ca46f8117b81a93f46a73112b5ec394378eb9c
Eya-Helali <eya.heleli@gmail.com>
2023-12-23 00:32:48 +0100

    todo-gateway

---
+++ todo-gateway/src/main/java/com/example/todogateway/TodoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.todogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoGatewayApplication.class, args);
+	}
+
+}
